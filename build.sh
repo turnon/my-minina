98c3ff00431549f8515d044d04c63c5b98bb4b33
ken <block24block@gmail.com>
2019-08-02 14:40:57 +0000

    auto detect file permission

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ ln -s /from/_posts /app/_posts \
   && ln -s /from/files /app/assets/files \
   && ln -s /to /app/_site \
   && jekyll build \
-  && find /app/_site/ -exec chown $OWN {} \;
+  && owner=`ls -ld /from | awk '{printf "%s:%s", $3, $4}'` \
+  && find /app/_site/ -exec chown $owner {} \;
